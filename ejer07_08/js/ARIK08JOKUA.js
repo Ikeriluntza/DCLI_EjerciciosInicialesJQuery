a8ce98368dfd37b7285eba1c1cee3f4fa8a7bbb1
ikertorre <ikertorre@DESKTOP-8FH3GTB>
2017-11-14 13:53:21 +0100

    Ejer08 - hace los cuadritos

--- ejer07_08/js/ARIK08JOKUA.js
+++ ejer07_08/js/ARIK08JOKUA.js
@@ -1,4 +1,6 @@
-palabra = "AZTERKETA";
+$palabra = "AZTERKET";
+
+
 
 $('#berria').click(function() {
 	$('#juego').css("display", "none");
@@ -10,15 +12,18 @@ $('#asmatu').click(function() {
 	$('#hitzberria').css("display", "none");
 	$('#juego').css("display", "block");
 
+	
+	for (i = 0; i < $palabra.length; i++) {
+        $('#cuadros').append(
+                '<div class="cuadritos" value="' + $palabra[i] + '"></div>');
+    }	
+	
 });
 
 $('#formuhitza').submit(function() {
 	alert($('#idatzih').val());
 	$palabra = $('#idatzih').val();
 
-	for (i = 0; i < $palabra.length; i++) {
-		$('body').HTML("<button>" + letras[i]) + "</button>";
-	}
 });
 
 
