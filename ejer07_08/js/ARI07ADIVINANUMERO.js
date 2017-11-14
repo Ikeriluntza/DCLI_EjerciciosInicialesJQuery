f98b9da54b9b5fe002c546f90338be679223904f
ikertorre <ikertorre@DESKTOP-8FH3GTB>
2017-11-14 12:36:55 +0100

    Ejer07 acabado.

--- ejer07_08/js/ARI07ADIVINANUMERO.js
+++ ejer07_08/js/ARI07ADIVINANUMERO.js
@@ -2,25 +2,26 @@ var numaleat = 0;
 var nummax = 50;
 var contador = 0;
 
-//Sacar un numero random
+// Sacar un numero random
 
-  $('#generarNum').click(function(){
-    numaleat = parseInt(Math.round(Math.random() * nummax));
-    alert("El numero aleatorio es: " + numaleat);
+$('#generarNum').click(function() {
+	numaleat = parseInt(Math.round(Math.random() * nummax));
+	alert("El numero aleatorio es: " + numaleat);
 
-    $('#divjuego').css("display", "block");
-  });
-  
-  $('#comprobar').click(function(){
-	    var numEscrito = parseInt($('#miNumero').val());
-  
-	    if (numEscrito == numaleat){
-	    	alert("Numero correcto");
-	    }else{
-	    	alert("Numero incorrecto");
-	    }
-  
-  
-  });
+	$('#divjuego').css("display", "block");
+});
 
-
+$('#comprobar').click(function() {
+	$valor = parseInt($('#miNumero').val());
+	if ($valor == $numero) {
+		alert('Has acertado');
+	} else {
+		$('#celda' + $contador + '').css("background-color", "red");
+		$contador = $contador + 1;
+		if ($valor < $numero) {
+			$('#texto').html('el numero es mayor');
+		} else {
+			$('#texto').html('el numero es menor');
+		}
+	}
+});
