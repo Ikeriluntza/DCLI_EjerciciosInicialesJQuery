83f07b32f87668bf98270c5c612640e28ed8e2cf
ikertorre <ikertorre@DESKTOP-8FH3GTB>
2017-11-14 13:35:50 +0100

    links de la nueva era

--- ejer07_08/js/ARIK08JOKUA.js
+++ ejer07_08/js/ARIK08JOKUA.js
@@ -11,3 +11,14 @@ $('#asmatu').click(function() {
 	$('#juego').css("display", "block");
 
 });
+
+$('#formuhitza').submit(function() {
+	alert($('#idatzih').val());
+	$palabra = $('#idatzih').val();
+
+	for (i = 0; i < $palabra.length; i++) {
+		$('body').HTML("<button>" + letras[i]) + "</button>";
+	}
+});
+
+
