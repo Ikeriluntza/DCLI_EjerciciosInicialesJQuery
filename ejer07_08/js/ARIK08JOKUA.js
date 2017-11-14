e523b30250499be2bfa68805b8cfb8041500a01b
ikertorre <ikertorre@DESKTOP-8FH3GTB>
2017-11-14 13:57:07 +0100

    Corregidito

--- ejer07_08/js/ARIK08JOKUA.js
+++ ejer07_08/js/ARIK08JOKUA.js
@@ -1,4 +1,4 @@
-$palabra = "AZTERKET";
+palabra = "AZTERKET";
 
 
 
@@ -13,16 +13,16 @@ $('#asmatu').click(function() {
 	$('#juego').css("display", "block");
 
 	
-	for (i = 0; i < $palabra.length; i++) {
+	for (i = 0; i < palabra.length; i++) {
         $('#cuadros').append(
-                '<div class="cuadritos" value="' + $palabra[i] + '"></div>');
+                '<div class="cuadritos" value="' + palabra[i] + '"></div>');
     }	
 	
 });
 
 $('#formuhitza').submit(function() {
 	alert($('#idatzih').val());
-	$palabra = $('#idatzih').val();
+	palabra = $('#idatzih').val();
 
 });
 
