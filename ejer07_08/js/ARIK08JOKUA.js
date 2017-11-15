e31e70286f213bad882a05e63533009f0866f667
ikertorre <ikertorre@DESKTOP-8FH3GTB>
2017-11-15 09:44:51 +0100

    Ejercicio08 - Completo

--- ejer07_08/js/ARIK08JOKUA.js
+++ ejer07_08/js/ARIK08JOKUA.js
@@ -1,29 +1,52 @@
-palabra = "AZTERKET";
-
-
+palabra = "AZTERKETA";
+longitud = palabra.length;
+nuevoelem = "";
+contador = 0;
 
 $('#berria').click(function() {
 	$('#juego').css("display", "none");
 	$('#hitzberria').css("display", "block");
-
 });
 
-$('#asmatu').click(function() {
-	$('#hitzberria').css("display", "none");
-	$('#juego').css("display", "block");
-
-	
-	for (i = 0; i < palabra.length; i++) {
-        $('#cuadros').append(
-                '<div class="cuadritos" value="' + palabra[i] + '"></div>');
-    }	
-	
-});
+$('#asmatu').click(
+		function() {
+			$('#hitzberria').css("display", "none");
+			$('#juego').css("display", "block");
+			for (i = 0; i < $palabra.length; i++) {
+				$('#cuadros').append(
+						'<div class="cuadritos" id="' + i + '" value="'
+								+ $palabra[i] + '"></div>');
+			}
+		});
 
 $('#formuhitza').submit(function() {
 	alert($('#idatzih').val());
-	palabra = $('#idatzih').val();
-
+	$palabra = $('#idatzih').val();
 });
 
+$('#formucomprobar').submit(function() {
+	$letra = $('#letra').val();
+	$palabraResul = $('#palabraadiv').val();
 
+	if ($palabraResul != '') {
+		alert($palabraResul);
+		if ($palabraResul == $palabra) {
+			alert('Has acertado en '+ contador + ' intentos.');
+			for (i = 0; i < $palabra.length; i++) {
+				$('#' + i + '').html($palabra[i]);
+			}
+		} else {
+			contador = contador + 1;
+		}
+	} else {
+		alert($letra);
+		for (i = 0; i < $palabra.length; i++) {
+			if ($letra == $palabra[i]) {
+				$('#' + i + '').append($letra);
+			}
+		}
+		contador = contador + 1;
+		$('#veces').val(contador);
+	}
+
+});
